0bd29aaf972a205427ed11242db17869fcbae618
Kelvince01 <themikrochip1@gmail.com>
2024-01-09 20:36:16 +0300

    feat: ngrx config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -25,6 +25,8 @@ import { getPerformance, providePerformance } from '@angular/fire/performance';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 import { environment } from '../environments/environment.development';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -52,6 +54,14 @@ export const appConfig: ApplicationConfig = {
       provideRemoteConfig(() => getRemoteConfig())
     ]),
     ScreenTrackingService,
-    UserTrackingService
+    UserTrackingService,
+    provideStore(),
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75 // maximum stack trace frames to be stored (in case trace option was provided as true)
+    })
   ]
 };
